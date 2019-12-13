e3f78684df9d38bda4766c3678f744aaa6592401
Rey Mercado <reymercado.usa@gmail.com>
2019-12-13 00:32:54 -0800

    examining the classList, a DOMTokenList, in document.links[0]  Please enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -14,5 +14,15 @@ val = document.contentType;
 
 val = document.forms;
 val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+val = document.forms[0].action;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList;
+
 
 console.log(val);
