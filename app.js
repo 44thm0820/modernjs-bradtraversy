a7c3ab45934d0e9f170ee9ed291f27df0841f9f3
Rey Mercado <reymercado.usa@gmail.com>
2019-12-13 00:27:57 -0800

    examining the document.forms html collection

--- app.js
+++ app.js
@@ -4,5 +4,15 @@ val = document;
 val = document.all; // gives you html collection
 val = document.all[2];
 val = document.all.length; // shows you 43 elements in the dom
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+
+val = document.forms;
+val = document.forms[0];
 
 console.log(val);
