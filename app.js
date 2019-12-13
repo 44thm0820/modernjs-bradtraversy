b02af85802a546030dad25943e763adb46eb6197
Rey Mercado <reymercado.usa@gmail.com>
2019-12-12 20:58:05 -0800

    going through window object. created a temporary div for a scrollbar to see scroll positions

--- app.js
+++ app.js
@@ -34,64 +34,99 @@
 // val = Math.floor(Math.random() * 20 + 1); // get random number betwen 1 and 20
 // val = Math.floor(Math.random() * 20 ); // get random number betwen 0 and 19
 
-//String object
-const firstName = 'George';
-const lastName = 'Washington';
-const age = 36;
-const str = 'Hello there my name is George';
-const tags = 'web design, web development, programming';
+// //String object
+// const firstName = 'George';
+// const lastName = 'Washington';
+// const age = 36;
+// const str = 'Hello there my name is George';
+// const tags = 'web design, web development, programming';
+
+// let val;
+
+// val = firstName + lastName;
+
+// //Concatenation
+// val = firstName + ' ' + lastName;
+
+// //Append
+// val = 'Abraham ';
+// val += 'Lincoln';
+// val = 'Hello, my name is ' + firstName + ' and I am ' + age;
+// //Escaping;
+// val = 'That\'s awesome, I can\'t wait';
+
+// //Length
+// val = firstName.length;
+
+// //concat()
+// val = firstName.concat(' ', lastName);
+
+// //change case
+// val = firstName.toUpperCase();
+// val = firstName.toLowerCase();
+
+// val = firstName[2];
+
+// //indexOf()
+// val = firstName.indexOf('r');
+// val = firstName.lastIndexOf('e');
+
+// //charAt()
+// val = firstName.charAt('2');
+// val = firstName.charAt(firstName.length - 1);
+
+// // substring()
+// val = firstName.substring(0,4);
+
+// // slice()
+// val = firstName.slice(0,4);
+// val = firstName.slice(-3);
+
+// // split()
+// /*comes in handy when you have a form on your blog post and you had them enter comma separated tags
+// */
+// val = str.split(' ');
+// val = tags.split(',');
+
+// //replace()
+// val = str.replace('George', 'Martha');
+
+// //includes()
+// val = str.includes('Hello');
+
+// console.log(val);
+
+// WINDOW METHODS / OBJECTS / PROPERTIES
+
+// Alert
+
+// alert ('Hello World');
+
+// Prompt - similar to alert except it takes an input
+// const input = prompt();
+// alert(input);
+
+// Confirm - used when deleting something - because user may be deleting by accident
+// if (confirm('Are you sure')) {
+//   console.log('YES'); // if you hit 'OK' button
+// } else {
+//   console.log('NO'); // if you hit 'Cancel' button
+// }
+
+// The above aren't the best looking things
+// I would suggest putting alerts if you put error message in form validation, I would suggest using the DOM and then outputting a nice looking bootstrap alert.  the above are the 90s type web applications, but they are there for you to use
 
 let val;
 
-val = firstName + lastName;
+// Outer height and width;
+val = window.outerHeight;
+val = window.outerWidth;
 
-//Concatenation
-val = firstName + ' ' + lastName;
+// Inner height and width;
+val = window.innerHeight;
+val = window.innerWidth;
 
-//Append
-val = 'Abraham ';
-val += 'Lincoln';
-val = 'Hello, my name is ' + firstName + ' and I am ' + age;
-//Escaping;
-val = 'That\'s awesome, I can\'t wait';
-
-//Length
-val = firstName.length;
-
-//concat()
-val = firstName.concat(' ', lastName);
-
-//change case
-val = firstName.toUpperCase();
-val = firstName.toLowerCase();
-
-val = firstName[2];
-
-//indexOf()
-val = firstName.indexOf('r');
-val = firstName.lastIndexOf('e');
-
-//charAt()
-val = firstName.charAt('2');
-val = firstName.charAt(firstName.length - 1);
-
-// substring()
-val = firstName.substring(0,4);
-
-// slice()
-val = firstName.slice(0,4);
-val = firstName.slice(-3);
-
-// split()
-/*comes in handy when you have a form on your blog post and you had them enter comma separated tags
-*/
-val = str.split(' ');
-val = tags.split(',');
-
-//replace()
-val = str.replace('George', 'Martha');
-
-//includes()
-val = str.includes('Hello');
+// Scroll points
+val = window.scrollY;
 
 console.log(val);
