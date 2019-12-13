0b789e7a7c92a9c62edfedb32941810b33cd6078
Rey Mercado <reymercado.usa@gmail.com>
2019-12-13 00:23:08 -0800

    examining number of elements in the dom by using length property of document.all

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ let val;
 val = document;
 val = document.all; // gives you html collection
 val = document.all[2];
+val = document.all.length; // shows you 43 elements in the dom
 
 console.log(val);
