c514e967f2afe2e64a556d3c59841d0ccc770663
Rey Mercado <reymercado.usa@gmail.com>
2019-12-12 19:43:45 -0800

    complete math function review

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 // alert('Hello');
-console.log('Hello World');
-console.log(123);
-console.log(true);
-var greeting = "Hello";
-console.log(greeting);
-console.log([1,2,3,4,]);
-console.log({a:1, b:2});
-console.table({a:1, b:2});
+// console.log('Hello World');
+// console.log(123);
+// console.log(true);
+// var greeting = "Hello";
+// console.log(greeting);
+// console.log([1,2,3,4,]);
+// console.log({a:1, b:2});
+// console.table({a:1, b:2});
 
-console.error('This is some error');
-console.clear();
-console.warn('This is a warning');
-console.time('Hello');
-  console.log('Hello World');
-  console.log('Hello World');
-  console.log('Hello World');
-  console.log('Hello World');
-  console.log('Hello World');
-console.timeEnd('Hello');
+// console.error('This is some error');
+// console.clear();
+// console.warn('This is a warning');
+// console.time('Hello');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+//   console.log('Hello World');
+// console.timeEnd('Hello');
 
+// Math Object
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8,2);
+val = Math.min(2,33,4,1,55,6,3,-2);
+val = Math.max(2,33,4,1,55,6,3,-2);
+val = Math.random();
+val = Math.floor(Math.random() * 20 + 1); // get random number betwen 1 and 20
+val = Math.floor(Math.random() * 20 ); // get random number betwen 0 and 19
+
+console.log(val);
