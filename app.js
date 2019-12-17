e77312b06ee44563495e3cc2174a783190d86d2f
Rey Mercado <reymercado.usa@gmail.com>
2019-12-16 18:30:50 -0800

    completed practice of replacing element using vanilla javaScript

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-// Create element
-const li = document.createElement('li');
+// REPLACE ELEMENT
 
-// Add class
-li.className = 'collection-item';
-
+// Create Element
+const newHeading = document.createElement('h2');
 // Add id
-li.id = 'new-item';
+newHeading.id = 'task-title';
+// New text node
+newHeading.appendChild(document.createTextNode('Task List'));
 
-// Add attribute
-li.setAttribute('title', 'New Item');
+// Get the old heading
+const oldHeading = document.getElementById('task-title');
+//Parent
+const cardAction = document.querySelector('.card-action');
 
-// Create text node and append
-li.appendChild(document.createTextNode('Hello World'));
+//Replace
+cardAction.replaceChild(newHeading, oldHeading);
 
-// Create new link element
-const link = document.createElement('a');
-// Add classes
-link.className = 'delete-item secondary-content';
-// Add icon html
-link.innerHTML = '<i class="fa fa-remove"></i>';
-
-// Append link into li
-li.appendChild(link);
-
-// Append li as child to ul
-document.querySelector('ul.collection').appendChild(li);
-console.log(li);
+console.log(newHeading);
