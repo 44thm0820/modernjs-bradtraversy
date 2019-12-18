1cff87ff634c1925c644e12ea4746fe15f6e7d5d
Rey Mercado <reymercado.usa@gmail.com>
2019-12-18 15:54:40 -0800

    going over mouseenter and mouseleave vs mouseover and mouseout

--- app.js
+++ app.js
@@ -1,45 +1,31 @@
-// document.querySelector('.clear-tasks')
-//   .addEventListener('click', function(e){
-//     console.log('Hello World');
-//     // e.preventDefault();
-//   });
+const clearBtn = document.querySelector('.clear-tasks');
+const card = document.querySelector('.card');
+const heading = document.querySelector('h5');
 
-//Having a named function is much neater, as shown below
-document.querySelector('.clear-tasks')
-  .addEventListener('click', onClick);
-function onClick(e) {
-  let val;
-  val = e;
-  //Event target element
-  val = e.target; //<a href="#" class="clear-tasks btn black">Clear Tasks</a>
-  val = e.target.id;//blank as it does not have an id
-  val = e.target.className; //clear-tasks btn black
-  val = e.target.classList;
-    /* DOMTokenList(3) ["clear-tasks", "btn", "black", value: "clear-tasks btn black"]
-    0: "clear-tasks"
-    1: "btn"
-    2: "black"
-    length: 3
-    value: "clear-tasks btn black"
-    __proto__: DOMTokenList */
-  // e.target.innerText = 'Hello'; // changes button text to Hello
+// //Click
+// clearBtn.addEventListener('click', runEvent);
 
-  //Event type
-  val = e.type; //click
+// //Doublelick
+// clearBtn.addEventListener('dblclick', runEvent);
 
-  // Timestamp
-  val = e.timeStamp;
+// //Mousedown
+// clearBtn.addEventListener('mousedown', runEvent);
+// //Mouseup
+// clearBtn.addEventListener('mouseup', runEvent);
+// //Mouseenter
+card.addEventListener('mouseenter', runEvent);
+// //Mouseleave
+card.addEventListener('mouseleave', runEvent);
+// //Mouseover
+card.addEventListener('mouseover', runEvent);
+// //Mouseout
+card.addEventListener('mouseout', runEvent);
 
-  //Coordinates event relative to the window
-  val = e.clientY;
-  val = e.clientX;
+//subtle difference with mouseover and mouseout... mouseenter and mouseleave only fire on the intitial parent element
 
-  //Coordinates event relative to the element
-  val = e.offsetY;
-  val = e.offsetX;
 
-  console.log(val);
+
+// Event Handler
+function runEvent(e) {
+  console.log(`EVENT TYPE: ${e.type}`);
 }
-//Now you can see the MouseEvent object in the console, with all its properties
-//most important property the target property.
-//target represents the element that the event actually happened on, in this case the a tag.
