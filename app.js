3a82234225e9f948c5d473941c22f00539987155
Rey Mercado <reymercado.usa@gmail.com>
2019-12-18 08:51:13 -0800

    viewing the mouseEvent in the console

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+// document.querySelector('.clear-tasks')
+//   .addEventListener('click', function(e){
+//     console.log('Hello World');
+//     // e.preventDefault();
+//   });
+
+//Having a named function is much neater, as shown below
 document.querySelector('.clear-tasks')
-  .addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('Hello World');
-  })
+  .addEventListener('click', onClick);
+function onClick(e) {
+  let val;
+  val = e;
+  console.log(val);
+}
+//Now you can see the MouseEvent object in the console, with all its properties
+//most important property the target property.
+//target represents the element that the event actually happened on, in this case the a tag.
