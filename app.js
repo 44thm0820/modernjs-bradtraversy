adb99bf8312d1a220f980ee0659d04e0924a3e45
Rey Mercado <reymercado.usa@gmail.com>
2019-12-19 10:15:48 -0800

    going over the beginning of Keyboard & Input Events

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-const clearBtn = document.querySelector('.clear-tasks');
-const card = document.querySelector('.card');
-const heading = document.querySelector('h5');
+const form = document.querySelector('form');
+const taskInput = document.getElementById('task');
 
-// //Click
-// clearBtn.addEventListener('click', runEvent);
+form.addEventListener('submit', runEvent);
 
-// //Doublelick
-// clearBtn.addEventListener('dblclick', runEvent);
-
-// //Mousedown
-// clearBtn.addEventListener('mousedown', runEvent);
-// //Mouseup
-// clearBtn.addEventListener('mouseup', runEvent);
-// //Mouseenter
-// card.addEventListener('mouseenter', runEvent);
-// // //Mouseleave
-// card.addEventListener('mouseleave', runEvent);
-// // //Mouseover
-// card.addEventListener('mouseover', runEvent);
-// // //Mouseout
-// card.addEventListener('mouseout', runEvent);
-
-//subtle difference with mouseover and mouseout... mouseenter and mouseleave only fire on the intitial parent element
-
-// Mousemove
-card.addEventListener('mousemove', runEvent);
-//mousemove good for games
-
-// Event Handler
 function runEvent(e) {
-  console.log(`EVENT TYPE: ${e.type}`);
-  heading.textContent = `MouseX: ${e.offsetX} MouseY: ${e.offsetY}`;
-  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+  console.log(`EVENT TYPE:${e.type}`);
 }
-
