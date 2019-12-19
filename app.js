adfd78b26bf55d0d9aa69b9d94e107a8bb977656
Rey Mercado <reymercado.usa@gmail.com>
2019-12-19 14:24:04 -0800

    demonstrating event bubbling

--- app.js
+++ app.js
@@ -1,51 +1,17 @@
-const form = document.querySelector('form');
-const taskInput = document.getElementById('task');
-const heading = document.querySelector('h5');
-const select = document.querySelector('select')
+// EVENT BUBBLING
 
-// Clear input
-taskInput.value = '';
+document.querySelector('.card-title').addEventListener('click', function(){
+  console.log('card title');
+})
 
-// form.addEventListener('submit', runEvent);
+document.querySelector('.card-content').addEventListener('click', function(){
+  console.log('card content');
+})
 
-// Keydown
-// taskInput.addEventListener('keydown', runEvent);
+document.querySelector('.card').addEventListener('click', function(){
+  console.log('card');
+})
 
-// Keyup
-// taskInput.addEventListener('keyup', runEvent);
-
-// Keypress
-// task.addEventListener('keypress', runEvent);
-
-// focus - when you click inside of an input and you set it to the focus mode
-// task.addEventListener('focus', runEvent);
-
-//blur - opposite of focus
-// task.addEventListener('blur', runEvent);
-
-//cut - fires event when user cuts text or command-x
-// task.addEventListener('cut', runEvent);
-
-//paste - fires event when user pastes text or command-v
-// task.addEventListener('paste', runEvent);
-
-//input - fires event when any edit happens, ie cut or paste,
-// task.addEventListener('input', runEvent);
-
-//demonstrating the select list - need to disable materialize so you can see select elements
-//Change
-// select.addEventListener('change', runEvent);
-
-
-function runEvent(e) {
-  console.log(`EVENT TYPE:${e.type}`);
-
-  // console.log(e.target.value);
-
-  // heading.innerText = e.target.value; //now the heading will include whatever i type.
-
-  //Get input value
-  // console.log(taskInput.value);
-
-  // e.preventDefault();
-}
+document.querySelector('.col').addEventListener('click', function(){
+  console.log('col');
+})
