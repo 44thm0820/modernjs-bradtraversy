1b14b11270a33d591d1bab18294e2fbc2ea269c4
Rey Mercado <reymercado.usa@gmail.com>
2019-12-19 17:48:51 -0800

    reviewing getItem from localStorage

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // set local storage item
-// localStorage.setItem('name', 'John');
+localStorage.setItem('name', 'John');
 
 // set session storage item
 // sessionStorage.setItem('name', 'Beth');
 
 // remove from storage
 // localStorage.removeItem('name');
+
+// get from storage
+const name = localStorage.getItem('name');
+console.log(name); //console will display John
