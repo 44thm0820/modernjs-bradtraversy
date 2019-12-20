13470fed1434aab3862306090f2167da53c3cbc5
Rey Mercado <reymercado.usa@gmail.com>
2019-12-19 17:45:01 -0800

    reviewing removeItem from localStorage

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // set local storage item
-localStorage.setItem('name', 'John');
+// localStorage.setItem('name', 'John');
 
 // set session storage item
-sessionStorage.setItem('name', 'Beth');
+// sessionStorage.setItem('name', 'Beth');
 
-
+// remove from storage
+// localStorage.removeItem('name');
